b00aa1b9cde4bae30c7ee584e23e3b339ee3f13e
ehivan24 <ehivan24@hotmail.com>
2016-07-06 22:16:30 -0400

    Analysing  for passengers.

--- data_analysis_titanic.py
+++ data_analysis_titanic.py
@@ -5,6 +5,7 @@ import numpy as np
 titanic_csv = pd.read_csv('titanic_data.csv')
 df_titanic = pd.DataFrame(titanic_csv)
 
+#print df_titanic.head()
 
 count_survivors = count_sex_male = count_sex_female = 0
 for survivor in df_titanic['Survived']:
@@ -42,11 +43,11 @@ for passenger_index, passenger in df_titanic.iterrows():
         age_not_registered[passenger_id] = 1
 
 
-print "Total Passengers: ", len(df_titanic['Survived'])
 print "Survivors: ",  count_survivors
-
 print "Male count:   ", count_sex_male
 print "Female count: ", count_sex_female
+
+print "Total Passengers: ", len(df_titanic['Survived'])
 
 print "Males less than 18 who survived: ", len(predictions_male_under_18)
 print "Females less than 18 who survived: ", len(predictions_female_under_18)
@@ -59,3 +60,79 @@ print "Age not Registered: ", len(age_not_registered)
 print "Total: ", len(not_survived) + len(not_under_18_females_survived) + \
                  len(not_under_18_males_survived) + len(predictions_male_under_18) + \
                  len(predictions_female_under_18) + len(age_not_registered)
+
+
+passenger_class_1 = {}
+passenger_class_2 = {}
+passenger_class_3 = {}
+for passenger_index_, passenger in df_titanic.iterrows():
+    passenger_id = passenger['PassengerId']
+    if passenger['Pclass'] == 1:
+        passenger_class_1[passenger_id] = 1
+    elif passenger['Pclass'] == 2:
+        passenger_class_2[passenger_id] = 1
+    elif passenger['Pclass'] == 3:
+        passenger_class_3[passenger_id] = 1
+"""
+    Classifies the passenger by class, 1st, 2nd, 3rd.
+"""
+
+print "Passengers 1st class: ", len(passenger_class_1)
+print "Passengers 2nd class: ", len(passenger_class_2)
+print "Passengers 3rd class: ", len(passenger_class_3)
+
+print "Total: ", len(passenger_class_1) + len(passenger_class_2) + len(passenger_class_3)
+
+
+"""
+    Passengers Per class who survived.
+"""
+
+passenger_first_class_survivor = {}
+passenger_second_class_survivor = {}
+passenger_third_class_survivor = {}
+
+passenger_first_class_not_survivor = {}
+passenger_second_class_not_survivor = {}
+passenger_third_class_not_survivor = {}
+
+for passenger_index__, passenger in df_titanic.iterrows():
+    passenger_id = passenger['PassengerId']
+    if passenger['Pclass'] == 1 and passenger['Survived'] == 1:
+        passenger_first_class_survivor[passenger_id] = 1
+    elif passenger['Pclass'] == 2 and passenger['Survived'] == 1:
+        passenger_second_class_survivor[passenger_id] = 1
+    elif passenger['Pclass'] == 3 and passenger['Survived'] == 1:
+        passenger_third_class_survivor[passenger_id] = 1
+    elif passenger['Pclass'] == 1 and passenger['Survived'] == 0:
+        passenger_first_class_not_survivor[passenger_id] = 1
+    elif passenger['Pclass'] == 2 and passenger['Survived'] == 0:
+        passenger_second_class_not_survivor[passenger_id] = 1
+    elif passenger['Pclass'] == 3 and passenger['Survived'] == 0:
+        passenger_third_class_not_survivor[passenger_id] = 1
+
+
+print "First Class Survivors: ", len(passenger_first_class_survivor)
+print "Second Class Survivors:", len(passenger_second_class_survivor)
+print "Third Class Survivors:", len(passenger_third_class_survivor)
+
+survived_per_class = len(passenger_first_class_survivor) + \
+                         len(passenger_second_class_survivor) + \
+                         len(passenger_third_class_survivor)
+print "Total Survivors: ", survived_per_class
+
+print "First Class Not Survivors:", len(passenger_first_class_not_survivor)
+print "Second Class Not Survivors: ", len(passenger_second_class_not_survivor)
+print "Third Class Not Survivors:", len(passenger_third_class_not_survivor)
+
+not_survived_per_class = len(passenger_first_class_not_survivor) + \
+                         len(passenger_second_class_not_survivor) + \
+                         len(passenger_third_class_not_survivor)
+
+print "Total Not Survivors: ", not_survived_per_class
+print "Total: ", not_survived_per_class + survived_per_class
+
+max_price = df_titanic['Fare'].max()
+print "Expensive ticket: ", max_price
+
+print df_titanic['Fare'].describe()
