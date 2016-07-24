a671b8698f1a60cf5343aae4d90aeddba505d0d9
ehivan24 <ehivan24@hotmail.com>
2016-07-24 18:39:22 -0400

    Daily commit : Cost function

--- weather_data_analysis.py
+++ weather_data_analysis.py
@@ -1,14 +1,21 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from ggplot import *
+import seaborn as sns
+sns.set()
 
 
-weather = pd.read_csv('turnstile_weather.csv')
+weather = pd.read_csv('weather_underground.csv')
 df_weather = pd.DataFrame(weather)
+print df_weather.keys()
+#plt.figure()
+#plt.plot(df_weather['heatingdegreedaysnormal'], df_weather['since1julheatingdegreedaysnormal'])
+#plt.show()
 
+print ggplot(df_weather, aes('since1julheatingdegreedaysnormal', 'heatingdegreedaysnormal')) \
+      + geom_point(color='blue') \
+      + geom_line(color='red') \
+      + ggtitle('since1julheatingdegreedaysnormal') \
+      + xlab('Year') + ylab('heatingdegreedaysnormal')
 
-def turnstile_df(df):
-    plt.figure()
-
-
-    return plt
